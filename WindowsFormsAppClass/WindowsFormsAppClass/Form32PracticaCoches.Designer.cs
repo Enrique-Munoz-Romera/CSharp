b490784f04d39c3f930175e065a859dd4c280438
Enrique Muñoz <enriquemunozromera@outlook.es>
2020-12-03 20:32:03 +0100

    03-12-20

--- WindowsFormsAppClass/WindowsFormsAppClass/Form32PracticaCoches.Designer.cs
+++ WindowsFormsAppClass/WindowsFormsAppClass/Form32PracticaCoches.Designer.cs
@@ -39,13 +39,15 @@
             this.btnSave = new System.Windows.Forms.Button();
             this.btnRead = new System.Windows.Forms.Button();
             this.btnImage = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnBefore = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pct1)).BeginInit();
             this.SuspendLayout();
             // 
             // pct1
             // 
             this.pct1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pct1.Location = new System.Drawing.Point(25, 165);
+            this.pct1.Location = new System.Drawing.Point(25, 182);
             this.pct1.Name = "pct1";
             this.pct1.Size = new System.Drawing.Size(208, 237);
             this.pct1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -57,15 +59,15 @@
             this.lblMarca.AutoSize = true;
             this.lblMarca.Location = new System.Drawing.Point(22, 20);
             this.lblMarca.Name = "lblMarca";
-            this.lblMarca.Size = new System.Drawing.Size(55, 18);
+            this.lblMarca.Size = new System.Drawing.Size(84, 29);
             this.lblMarca.TabIndex = 1;
             this.lblMarca.Text = "Marca";
             // 
             // txtMarca
             // 
-            this.txtMarca.Location = new System.Drawing.Point(25, 41);
+            this.txtMarca.Location = new System.Drawing.Point(25, 50);
             this.txtMarca.Name = "txtMarca";
-            this.txtMarca.Size = new System.Drawing.Size(208, 24);
+            this.txtMarca.Size = new System.Drawing.Size(208, 33);
             this.txtMarca.TabIndex = 2;
             // 
             // lblModelo
@@ -73,40 +75,40 @@
             this.lblModelo.AutoSize = true;
             this.lblModelo.Location = new System.Drawing.Point(22, 88);
             this.lblModelo.Name = "lblModelo";
-            this.lblModelo.Size = new System.Drawing.Size(64, 18);
+            this.lblModelo.Size = new System.Drawing.Size(101, 29);
             this.lblModelo.TabIndex = 3;
             this.lblModelo.Text = "Modelo";
             // 
             // txtModelo
             // 
-            this.txtModelo.Location = new System.Drawing.Point(25, 109);
+            this.txtModelo.Location = new System.Drawing.Point(25, 120);
             this.txtModelo.Name = "txtModelo";
-            this.txtModelo.Size = new System.Drawing.Size(208, 24);
+            this.txtModelo.Size = new System.Drawing.Size(208, 33);
             this.txtModelo.TabIndex = 4;
             // 
             // lblCoches
             // 
             this.lblCoches.AutoSize = true;
-            this.lblCoches.Location = new System.Drawing.Point(329, 24);
+            this.lblCoches.Location = new System.Drawing.Point(773, 18);
             this.lblCoches.Name = "lblCoches";
-            this.lblCoches.Size = new System.Drawing.Size(66, 18);
+            this.lblCoches.Size = new System.Drawing.Size(101, 29);
             this.lblCoches.TabIndex = 5;
             this.lblCoches.Text = "Coches";
             // 
             // lstCoches
             // 
             this.lstCoches.FormattingEnabled = true;
-            this.lstCoches.ItemHeight = 18;
-            this.lstCoches.Location = new System.Drawing.Point(278, 45);
+            this.lstCoches.ItemHeight = 29;
+            this.lstCoches.Location = new System.Drawing.Point(713, 50);
             this.lstCoches.Name = "lstCoches";
-            this.lstCoches.Size = new System.Drawing.Size(235, 220);
+            this.lstCoches.Size = new System.Drawing.Size(235, 207);
             this.lstCoches.TabIndex = 6;
             // 
             // btnIn
             // 
-            this.btnIn.Location = new System.Drawing.Point(278, 302);
+            this.btnIn.Location = new System.Drawing.Point(697, 274);
             this.btnIn.Name = "btnIn";
-            this.btnIn.Size = new System.Drawing.Size(81, 26);
+            this.btnIn.Size = new System.Drawing.Size(123, 37);
             this.btnIn.TabIndex = 7;
             this.btnIn.Text = "Insertar";
             this.btnIn.UseVisualStyleBackColor = true;
@@ -114,9 +116,9 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(380, 302);
+            this.btnSave.Location = new System.Drawing.Point(839, 274);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(98, 26);
+            this.btnSave.Size = new System.Drawing.Size(109, 37);
             this.btnSave.TabIndex = 8;
             this.btnSave.Text = "Save File";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -124,9 +126,9 @@
             // 
             // btnRead
             // 
-            this.btnRead.Location = new System.Drawing.Point(380, 346);
+            this.btnRead.Location = new System.Drawing.Point(839, 329);
             this.btnRead.Name = "btnRead";
-            this.btnRead.Size = new System.Drawing.Size(98, 29);
+            this.btnRead.Size = new System.Drawing.Size(109, 41);
             this.btnRead.TabIndex = 9;
             this.btnRead.Text = "Load File";
             this.btnRead.UseVisualStyleBackColor = true;
@@ -134,19 +136,41 @@
             // 
             // btnImage
             // 
-            this.btnImage.Location = new System.Drawing.Point(278, 346);
+            this.btnImage.Location = new System.Drawing.Point(697, 329);
             this.btnImage.Name = "btnImage";
-            this.btnImage.Size = new System.Drawing.Size(81, 29);
+            this.btnImage.Size = new System.Drawing.Size(123, 41);
             this.btnImage.TabIndex = 10;
             this.btnImage.Text = "Image";
             this.btnImage.UseVisualStyleBackColor = true;
             this.btnImage.Click += new System.EventHandler(this.btnImage_Click);
             // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(839, 381);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(109, 38);
+            this.btnNext.TabIndex = 11;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnBefore
+            // 
+            this.btnBefore.Location = new System.Drawing.Point(697, 381);
+            this.btnBefore.Name = "btnBefore";
+            this.btnBefore.Size = new System.Drawing.Size(123, 38);
+            this.btnBefore.TabIndex = 12;
+            this.btnBefore.Text = "Before";
+            this.btnBefore.UseVisualStyleBackColor = true;
+            this.btnBefore.Click += new System.EventHandler(this.btnBefore_Click);
+            // 
             // Form32PracticaCoches
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(619, 439);
+            this.ClientSize = new System.Drawing.Size(1037, 607);
+            this.Controls.Add(this.btnBefore);
+            this.Controls.Add(this.btnNext);
             this.Controls.Add(this.btnImage);
             this.Controls.Add(this.btnRead);
             this.Controls.Add(this.btnSave);
@@ -181,5 +205,7 @@
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnRead;
         private System.Windows.Forms.Button btnImage;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnBefore;
     }
 }
--- WindowsFormsAppClass/WindowsFormsAppClass/Form32PracticaCoches.cs
+++ WindowsFormsAppClass/WindowsFormsAppClass/Form32PracticaCoches.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,14 +15,18 @@ namespace WindowsFormsAppClass
 {
     public partial class Form32PracticaCoches : Form
     {
-        ColeccionProductos coches;
+        ColeccionCoches coches;
         XmlSerializer serial;
+        int indice;
+        string path;
 
         public Form32PracticaCoches()
         {
             InitializeComponent();
-            this.coches = new ColeccionProductos();
-            this.serial = new XmlSerializer(typeof(Coche));
+            this.coches = new ColeccionCoches();
+            this.serial = new XmlSerializer(typeof(ColeccionCoches));
+            this.indice = 0;
+            this.path = "../../Images/simpson.jpg";
         }
 
         private void btnIn_Click(object sender, EventArgs e)
@@ -29,6 +34,12 @@ namespace WindowsFormsAppClass
             Coche auto = new Coche();
             auto.Marca = txtMarca.Text;
             auto.Modelo = txtModelo.Text;
+            Image imagen = Image.FromFile(path);
+            using (var ms = new MemoryStream())
+            {
+                imagen.Save(ms, imagen.RawFormat);
+                auto.Imagen = ms.ToArray();
+            }
             this.coches.Add(auto);
             this.txtMarca.Clear();
             this.txtMarca.Focus();
@@ -41,24 +52,59 @@ namespace WindowsFormsAppClass
             this.lstCoches.Items.Clear();
             foreach(Coche auto in this.coches)
             {
-                this.lstCoches.Items.Add(auto.Marca);
-                this.txtModelo.Text = auto.Modelo;
+                this.lstCoches.Items.Add(auto.Marca + " --> " + auto.Modelo);
+                //this.lstCoches.Items.Add(auto.Modelo);
+                this.pct1.Image = Image.FromFile(path);
             }
+            this.lstCoches.SelectedIndex = this.indice;
+        }
+
+        private async void btnSave_Click(object sender, EventArgs e)
+        {
+            StreamWriter writer = new StreamWriter("listaCoches2.xml");
+            this.serial.Serialize(writer,coches);
+            await writer.FlushAsync();
+            writer.Close();
+            this.coches.Clear();
+            this.lstCoches.Items.Clear();            
+            MessageBox.Show("Save successfuly");
         }
 
         private void btnRead_Click(object sender, EventArgs e)
         {
+            StreamReader reader = new StreamReader("listaCoches2.xml");
+            //Realizamos el casting para la desserializacion
+            this.coches = (ColeccionCoches)this.serial.Deserialize(reader);
+            reader.Close();
+            this.PintarCoche();
 
-        }
-
-        private void btnSave_Click(object sender, EventArgs e)
-        {
-
-        }
+        }       
 
         private void btnImage_Click(object sender, EventArgs e)
         {
+            StreamReader reader = new StreamReader("listaCoches2.xml");
+            this.coches = (ColeccionCoches)this.serial.Deserialize(reader);
+            reader.Close();
+        }
 
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            this.indice += 1;  
+            if(this.indice >= this.coches.Count)
+            {
+                this.indice = 0;
+            }
+            this.PintarCoche();
+        }
+
+        private void btnBefore_Click(object sender, EventArgs e)
+        {
+            this.indice -= 1;
+            if(this.indice < 0)
+            {
+                this.indice = this.coches.Count - 1 ;
+            }
+            this.PintarCoche();
         }
     }
 }
